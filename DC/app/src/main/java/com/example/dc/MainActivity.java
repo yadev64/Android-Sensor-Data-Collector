3ad5a8fa438fb9f08ae21e5b01800e4e36e4d8f0
Yadev Jayachandran <yadev64@gmail.com>
2020-04-20 23:01:31 +0530

    Update MainActivity.java

--- DC/app/src/main/java/com/example/dc/MainActivity.java
+++ DC/app/src/main/java/com/example/dc/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.dc;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
@@ -15,7 +17,9 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -33,9 +37,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         sensorManagers = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometor = sensorManagers.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -44,14 +50,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         senGyroscope = sensorManagers.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         sensorManagers.registerListener(this, senGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 ////        LocationListener locationListener = new
 //        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 //
 //            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MYLOCATION );
 //            return;
 //        }
-//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+
+                Toast.makeText(getApplicationContext(), "Error getting permissions", Toast.LENGTH_SHORT);
+            }else {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, MYLOCATION);
+            }
+
+
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, (LocationListener) this);
 
 
     }
@@ -84,17 +101,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
-
+            Log.d("Latitude", "Status");
         }
 
         @Override
         public void onProviderEnabled(String provider) {
-
+            Log.d("Latitude", "Enable");
         }
 
         @Override
         public void onProviderDisabled(String provider) {
-
+            Log.d("Latitude", "Disable");
         }
     };
 
