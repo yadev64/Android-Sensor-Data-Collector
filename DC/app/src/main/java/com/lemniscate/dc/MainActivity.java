fa6428de170b44cdfded84e6f3ed5b6c558b86f6
Yadev Jayachandran <yadev64@gmail.com>
2020-04-27 14:31:44 +0530

    re structured the code

--- DC/app/src/main/java/com/lemniscate/dc/MainActivity.java
+++ DC/app/src/main/java/com/lemniscate/dc/MainActivity.java
@@ -199,6 +199,88 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         });
     }
 
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+
+
+        Sensor mySensor = sensorEvent.sensor;
+        Sensor GSensor = sensorEvent.sensor;
+
+
+        if (GSensor.getType()==Sensor.TYPE_GYROSCOPE){
+            float gx = sensorEvent.values[0];
+            float gy = sensorEvent.values[1];
+            float gz = sensorEvent.values[2];
+
+            long currentTime = System.currentTimeMillis();
+            if ((currentTime - lastUpdate_gyro > 300)) {
+
+                lastUpdate_gyro = currentTime;
+
+                {
+                    GetNewLocation();                                   //Just to update the location ;P
+
+
+                    String sX = Float.toString(gx);
+                    TextView text = (TextView) findViewById(R.id.gx);
+                    text.setText(sX);
+
+                    String sY = Float.toString(gy);
+                    text = (TextView) findViewById(R.id.gy);
+                    text.setText(sY);
+
+                    String sZ = Float.toString(gz);
+                    text = (TextView) findViewById(R.id.gz);
+                    text.setText(sZ);
+
+                    fileString = fileString + sX + ", " + sY + ", " + sZ + ", ";
+                }
+            }
+        }
+
+        if(mySensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long currentTime = System.currentTimeMillis();
+
+            if ((currentTime - lastUpdate) > 300){
+                long timeDiff = currentTime - lastUpdate;
+                lastUpdate = currentTime;
+
+                {
+                    String sX = Float.toString(x);
+                TextView text = (TextView) findViewById(R.id.ax);
+                text.setText(sX);
+
+                    ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+                    int progress = (int) (((-1 * x) + 10) * 10000);
+                    progressBar.setProgress(progress);
+
+                String sY = Float.toString(y);
+                text = (TextView) findViewById(R.id.ay);
+                text.setText(sY);
+
+                String sZ = Float.toString(z);
+                text = (TextView) findViewById(R.id.az);
+                text.setText(sZ);
+
+                fileString = fileString + sX + ", " + sY + ", " + sZ + "\n";
+                FileWriters(fileString);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
     public void ChangeTheme(){
         if(c==0){
 
@@ -352,86 +434,5 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             CardView cv = (CardView) findViewById(R.id.cardView);
             cv.setCardBackgroundColor(getResources().getColor(R.color.dark_mode_card));
         }
-    }
-
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-
-
-
-        Sensor mySensor = sensorEvent.sensor;
-        Sensor GSensor = sensorEvent.sensor;
-
-
-        if (GSensor.getType()==Sensor.TYPE_GYROSCOPE){
-            float gx = sensorEvent.values[0];
-            float gy = sensorEvent.values[1];
-            float gz = sensorEvent.values[2];
-
-            long currentTime = System.currentTimeMillis();
-            if ((currentTime - lastUpdate_gyro > 300)) {
-
-                lastUpdate_gyro = currentTime;
-
-                {
-                    GetNewLocation();                                   //Just to update the location ;P
-
-
-                    String sX = Float.toString(gx);
-                    TextView text = (TextView) findViewById(R.id.gx);
-                    text.setText(sX);
-
-                    String sY = Float.toString(gy);
-                    text = (TextView) findViewById(R.id.gy);
-                    text.setText(sY);
-
-                    String sZ = Float.toString(gz);
-                    text = (TextView) findViewById(R.id.gz);
-                    text.setText(sZ);
-
-                    fileString = fileString + sX + ", " + sY + ", " + sZ + ", ";
-                }
-            }
-        }
-
-        if(mySensor.getType()==Sensor.TYPE_ACCELEROMETER){
-            float x = sensorEvent.values[0];
-            float y = sensorEvent.values[1];
-            float z = sensorEvent.values[2];
-
-            long currentTime = System.currentTimeMillis();
-
-            if ((currentTime - lastUpdate) > 300){
-                long timeDiff = currentTime - lastUpdate;
-                lastUpdate = currentTime;
-
-                {
-                    String sX = Float.toString(x);
-                TextView text = (TextView) findViewById(R.id.ax);
-                text.setText(sX);
-
-                    ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
-                    int progress = (int) (((-1 * x) + 10) * 10000);
-                    progressBar.setProgress(progress);
-
-                String sY = Float.toString(y);
-                text = (TextView) findViewById(R.id.ay);
-                text.setText(sY);
-
-                String sZ = Float.toString(z);
-                text = (TextView) findViewById(R.id.az);
-                text.setText(sZ);
-
-                fileString = fileString + sX + ", " + sY + ", " + sZ + "\n";
-                FileWriters(fileString);
-                }
-            }
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 }
